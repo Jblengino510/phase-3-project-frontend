1a640bbcd408372fc537bc7b6e72b0e13a951746
Joshua <j.blengino@yahoo.com>
2021-09-01 23:22:21 -0700

    need to fix AppBar

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,3 @@
-import NavBar from './NavBar'
 import PostCard from './PostCard'
 import Grid from '@material-ui/core/Grid'
 import Link from '@material-ui/core/Link'
@@ -9,14 +8,11 @@ function PostList({ posts }) {
         <Grid key={post.id} item spacing={3} xs={12} md={6} lg={4}><PostCard album_name={post.album_name} image_url={post.image_url} artist={post.artist} genre={post.genre} tracklist={post.tracklist} post={post} /></Grid>)
 
     return (
-        <>
             <div className="postList">
-                    <NavBar />
                     <Grid container spacing={3}>
                         {renderedPosts}
                     </Grid>
             </div>
-        </>
     )
 }
 
