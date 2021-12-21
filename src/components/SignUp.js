58e3ad71ef0396f20a6f59d997981d03f5c82ef7
Joshua <j.blengino@yahoo.com>
2021-12-20 22:50:10 -0800

    signup form styled

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,51 @@
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider';
 import { useState } from "react"
 import { useHistory } from "react-router-dom"
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100vh',
+        color: 'white'
+    },
+    signUpForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '30%',
+        marginTop: '100px'
+    },
+    btn: {
+        backgroundColor: '#815A34',
+        padding: '25px',
+        width: '30%',
+        color: 'white' 
+    }, 
+    divider: {
+        backgroundColor: '#252525',
+        width: '5%',
+        height: '3px',
+        marginTop: '40px'
+    },
+    loginText: {
+        '&:hover': {cursor: 'pointer', color: 'white'},
+        color: '#815A34',
+    }
+})
 
 function SignUp({ setAllUsers }) {
     const [ formData, setFormData ] = useState({
         user_name: ""
     })
     const history = useHistory()
+    const classes= useStyles()
 
     function handleFormChange(e) {
         setFormData({
@@ -30,9 +68,12 @@ function SignUp({ setAllUsers }) {
     }
 
     return (
-        <div className="signUpForm">
-            <h1 style={{color: 'white'}}>Create Your Free Crate. Account</h1>
-            <form onSubmit={handleSignUpSubmit}>
+        <div className={classes.container}>
+            <Typography variant='h3' style={{color: 'white', marginTop: '100px'}}>
+                Create Your Free Crate. Account
+            </Typography>
+            <Divider className={classes.divider}/>
+            <form onSubmit={handleSignUpSubmit} className={classes.signUpForm}>
                 <TextField 
                 id="standard-full-width"
                 fullWidth
@@ -46,10 +87,15 @@ function SignUp({ setAllUsers }) {
                 />
                 <br></br>
                 <br></br>
-                <Button type="submit" variant="contained" style={{backgroundColor: '#815A34', color: 'white'}}>
+                <Button type="submit" variant="contained" className={classes.btn}>
                     Sign Up
                 </Button>
             </form>
+            <Typography variant='body1' style={{marginTop: '20px'}}>
+                <strong>Already have an account?</strong>
+                &nbsp;
+                <strong className={classes.loginText} onClick={() => history.push('/login')}>Login</strong>
+            </Typography>
         </div>
     )
 }
